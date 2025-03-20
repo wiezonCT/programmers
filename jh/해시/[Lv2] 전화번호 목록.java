319674e0078811a4cb87cecfc5b819a931c775bd
jhlee <jhlee@wiezon.com>
2025-03-20 17:06:58 +0900

    해시

---
+++ jh/해시/[Lv2] 전화번호 목록.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Solution {
+
+    public static void main(String[] args) {
+        System.out.print(solution(new String[]{"119", "97674223", "1195524421"}));
+    }
+
+    public static boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
+
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if (phone_book[i + 1].startsWith(phone_book[i])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
